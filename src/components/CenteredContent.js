12753cedae91b1a1f5211a521e82903bb6e0b61e
eladdavid5 <elad.david5@gmail.com>
2023-12-25 23:41:03 +0200

    CenteredContent Update

--- src/components/CenteredContent.js
+++ src/components/CenteredContent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '@/styles/CenteredContent.module.css'; // Import the CSS module
+import styles from '@/styles/CenteredContent.module.css'; 
 
 const CenteredContent = ({ children }) => {
   return <div className={styles.centered}>{children}</div>;
